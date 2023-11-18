f17ca96ddca50555202d7f46031a39088521aa64
meaxzy <olayinkawu@gmail.com>
2023-11-18 13:42:00 -0800

    add: bubble sort debugging

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -3,18 +3,6 @@
 #include "sort.h"
 
 /**
- * swap - function that sways the positions of 2 integers
- * @first_int: pointer to the first integer
- * @second_int: pointer to the 2nd integer
- */
-void swap(int *first_int, int *second_int)
-{
-	int temp = *first_int;
-	*first_int = *second_int;
-	*second_int = temp;
-}
-
-/**
  * bubble_sort - sorts an array of intergers in
  * ascending order using Bubble Sort
  * @array: The array to be sorted
@@ -26,7 +14,7 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
         int temp;
-        int flag;
+        int flagged;
 	
 	if (array == NULL || size <= 1)
 	{
@@ -35,7 +23,7 @@ void bubble_sort(int *array, size_t size)
 	
 	for (i = 0; i < size - 1; i++)
 	{
-		flag = 0;
+		flagged = 0;
 
 		for (j = 0; j < size - i - 1; j++)
 		{
@@ -45,15 +33,14 @@ void bubble_sort(int *array, size_t size)
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
-				swap(&array[j], &array[j + 1]);
 				print_array(array, size);
 
 				/* Set the flag to indicate a swap was made */
-				flag = 1;
+				flagged = 1;
 			}
 		}
 		/* if no swaps were made, the array is already sorted */
-		if (flag == 0)
+		if (flagged == 0)
 		{
 			break;
 		}
