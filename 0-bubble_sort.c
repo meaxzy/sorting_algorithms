da11adb746ef0be08a909da16d119dbd38977653
meaxzy <olayinkawu@gmail.com>
2023-11-18 13:53:30 -0800

    add: bubble sort debugging

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,4 +1,3 @@
-
 #include "sort.h"
 
 /**
@@ -13,14 +12,14 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
-        int temp;
-        int flagged;
-	
+	int temp;
+	int flagged;
+
 	if (array == NULL || size <= 1)
 	{
 		return;
 	}
-	
+
 	for (i = 0; i < size - 1; i++)
 	{
 		flagged = 0;
