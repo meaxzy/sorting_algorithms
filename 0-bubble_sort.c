80b13c25d2d3997de2a54ca0064a75350a56e079
meaxzy <olayinkawu@gmail.com>
2023-11-18 13:10:53 -0800

    add: bubble sort debugging

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "sort.h"
+
 /**
  * bubble_sort - sorts an array of intergers in
  * ascending order using Bubble Sort
@@ -11,7 +12,7 @@
 
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j, k;
+	size_t i, j;
 	int temp;
 	int flag;
 
@@ -27,7 +28,7 @@ void bubble_sort(int *array, size_t size)
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
-				/* Print array after each swap */
+				/* Print array after each swap
 				for (k = 0; k < size; k++)
 				{
 					printf("%d", array[k]);
@@ -36,7 +37,7 @@ void bubble_sort(int *array, size_t size)
 						printf(", ");
 					}
 				}
-				printf("\n");
+				printf("\n"); */
 
 				/* Set the flag to indicate a swap was made */
 				flag = 1;
